9bdb6a5b30623ad5d70d9f47b4ce237ceb74e995
ajowyang <andrewowyang875@gmail.com>
2025-01-24 18:38:11 -0800

    completed 101

---
+++ multiples_3_and_5.py
@@ -0,0 +1,13 @@
+def multisum(num):
+	result = 0
+	for i in range(1, num + 1):
+		if i % 5 == 0 or i % 3 == 0:
+			result += i
+			
+	return result
+
+# These examples should all print True
+print(multisum(3) == 3)
+print(multisum(5) == 8)
+print(multisum(10) == 33)
+print(multisum(1000) == 234168)
